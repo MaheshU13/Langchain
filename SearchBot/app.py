0f4508e01b200cbcde3a71fcf80a25b592a65ac8
Mahesh Just <153098038+MaheshU13@users.noreply.github.com>
2024-07-20 19:27:47 +0530

    latest

--- SearchBot/app.py
+++ SearchBot/app.py
@@ -1,50 +1,48 @@
-# Q&A Chatbot
-#from langchain.llms import OpenAI
-
-from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
-
 import streamlit as st
-import os
-import pathlib
 import textwrap
-
 import google.generativeai as genai
 
-from IPython.display import display
 from IPython.display import Markdown
 
-
 def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# API key
+API_KEY = "AIzaSyBxTLi6zZyorJqsH-lJgaATaXil0o4QOjA"
+genai.configure(api_key=API_KEY)
 
-## Function to load OpenAI model and get respones
+## Function to load Gemini model and get responses
 
 def get_gemini_response(question):
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(question)
     return response.text
 
-##initialize our streamlit app
+## Initialize our Streamlit app
 
-st.set_page_config(page_title="Q&A Demo")
+st.set_page_config(page_title="Q&A BOT")
 
-st.header("Gemini Application")
+st.header("Question Answering Bot")
 
-input=st.text_input("Input: ",key="input")
+input_text = st.text_input("Input: ", key="input")
 
+submit = st.button("Ask the question")
 
-submit=st.button("Ask the question")
-
-## If ask button is clicked
+## If button is clicked
 
 if submit:
-    
-    response=get_gemini_response(input)
-    st.subheader("The Response is")
-    st.write(response)
+    if input_text.strip():  # Check if the input is not empty or just whitespace
+        response = get_gemini_response(input_text)
+        st.subheader("The Response is")
+        st.write(response)
+    else:
+        st.warning("Please enter a question before submitting.")
+
+logo_path = r"logo4.jpg"  
+
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.image(logo_path, width=50)
+with col2:
+    st.markdown(" © 2024 Mahesh U")
